84bf6e8d2d9fc4c4a759e00d57d69fc6a23a01fd
Igor Vieira <igorfv@outlook.com>
2021-12-09 19:57:07 -0300

    Test tailwind

--- src/libs/header/index.svelte
+++ src/libs/header/index.svelte
@@ -1,11 +1,11 @@
 <svelte:head>
-  <title>Just another TODO</title>
+  <title>Just another TODO app</title>
 </svelte:head>
 
 <header class="bg-green-200 border-b border-slate-400 mb-8 py-4">
   <div class="max-w-screen-xl mx-auto">
     <div class="mx-4">
-      <h1 class="text-4xl text-green-900 font-extralight text-center">Just another TODO</h1>
+      <h1 class="text-4xl text-green-900 font-extralight text-center">Just another TODO app</h1>
     </div>
   </div>
 </header>
